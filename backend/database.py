03c9d188a8107bac920edb0ea2d5573375c3114f
Michał Ślęzak <realslezak.michal@gmail.com>
2025-12-26 11:40:38 +0100

    cleanup

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,6 @@ sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False} # allows multiple threads to access the db at the time
-
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
 def create_db_and_tables():
