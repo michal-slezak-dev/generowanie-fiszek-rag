2c94cf3e3f91f500a72f8ea39f40c1ec776b33ad
Michał Ślęzak <realslezak.michal@gmail.com>
2026-01-06 12:25:01 +0100

    enable cascade delete

--- backend/models.py
+++ backend/models.py
@@ -26,7 +26,7 @@ class Deck(SQLModel, table=True):
     user_id : Optional[int] = Field(default=None, foreign_key='user.id')
     user : Optional[User] = Relationship(back_populates='decks')
     
-    flashcards : List['Flashcard'] = Relationship(back_populates='deck')
+    flashcards : List['Flashcard'] = Relationship(back_populates='deck', cascade_delete=True)
 
 class Flashcard(SQLModel, table=True):
     id : Optional[int] = Field(default=None, primary_key=True)
@@ -38,15 +38,15 @@ class Flashcard(SQLModel, table=True):
     repetitions: int = Field(default=0)
     next_review_date: Optional[datetime] = Field(default_factory=lambda: datetime.now(timezone.utc))
 
-    deck_id : Optional[int] = Field(default_factory=None, foreign_key='deck.id')
+    deck_id : Optional[int] = Field(default_factory=None, foreign_key='deck.id', ondelete="CASCADE")
     deck : Optional[Deck] = Relationship(back_populates='flashcards')
 
-    review_logs: List["ReviewLog"] = Relationship(back_populates="flashcard")
+    review_logs: List["ReviewLog"] = Relationship(back_populates="flashcard", cascade_delete=True)
 
 class ReviewLog(SQLModel, table=True):
     """Tracks history of reviews for analytics"""
     id: Optional[int] = Field(default=None, primary_key=True)
-    flashcard_id: int = Field(foreign_key="flashcard.id")
+    flashcard_id: int = Field(foreign_key="flashcard.id", ondelete="CASCADE")
     
     review_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     grade: int  # user rating (0-5)
@@ -56,3 +56,6 @@ class ReviewLog(SQLModel, table=True):
     resulting_easiness_factor: float
     
     flashcard: Optional[Flashcard] = Relationship(back_populates="review_logs")
+
+
+# https://sqlmodel.tiangolo.com/tutorial/relationship-attributes/cascade-delete-relationships/#using-cascade_delete-or-ondelete
